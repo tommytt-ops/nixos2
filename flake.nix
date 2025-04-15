85aa7a41c09d320dc7e2aba28aaae796b32eba78
tommytt-ops <tommytt721@gmail.com>
2025-04-15 15:37:49 +0200

    ops

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    base-flake.url = "github:tommytt-ops/nixos";
+    baseFlake.url = "github:tommytt-ops/nixos";
   };
 
   outputs = { self, nixpkgs, tommy-config }:
     let
       laptopConfig = tommy-config.nixosConfigurations.laptop;
-      basePy311 = base-flake.devShells."x86_64-linux".python311;
+      basePy311 = baseFlake.devShells."x86_64-linux".python311;
     in {
       nixosConfigurations.myLaptopSystem = laptopConfig;
       devShells."x86_64-linux".python311v1 = pkgs.mkShell {
